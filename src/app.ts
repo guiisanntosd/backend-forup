3d929e17d5bfd51213439086c271ec8fdf1ff2d8
Guilherme Santos <guilherme.snt99@gmail.com>
2021-06-14 10:18:47 -0300

    adicionando cors

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import errorHandler from './errors/handler'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: 'https://agenciaforup.com/' }))
 app.use(express.json())
 app.use(routes)
 
